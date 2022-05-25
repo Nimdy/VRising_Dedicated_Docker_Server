a61c3ecaed6237a8ed3f7aacbbd3a08c14da6a7b
Zerobandwidth <nimdavirus@gmail.com>
2022-05-25 12:44:09 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -122,7 +122,7 @@ cp /etc/skel/.bashrc /home/steam/.bashrc
 cp /etc/skel/.profile /home/steam/.profile
 cd ~steam
 git clone https://github.com/Nimdy/VRising_Dedicated_Docker_Server.git
-cd VRising_Dedicated_Server
+cd VRising_Dedicated_Docker_Server
 chmod +x ./* -R
 mkdir -p data/save-data
 sudo docker build . -t vrisingdedserver/wine64:v20052022
